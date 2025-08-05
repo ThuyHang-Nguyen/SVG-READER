0a8737b9b6e455cb68aa8ee09a82595f2655eaa6
ThuyHang-Nguyen <nguyenhang180206@gmail.com>
2025-08-05 21:19:57 +0700

    Add note in Trans.h

--- Trans.h
+++ Trans.h
@@ -1,4 +1,5 @@
-﻿#pragma once
+//Declare transform
+#pragma once
 #include"Base.h"
 class multi_transform {
 public:
@@ -9,5 +10,6 @@ public:
 void transform_image(Graphics& graphics, float angle, float width, float height, float scroll_x, float scroll_y, float scale);
 void read_transform(string value, multi_transform& tr);
 void apply_transform(LinearGradientBrush& lgbrush, multi_transform trans);
+
 
 
