dd46c7f7836973e0d9e0e3bf911734970347a250
ThuyHang-Nguyen <nguyenhang180206@gmail.com>
2025-08-05 21:15:48 +0700

    Add files via upload

---
+++ Trans.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include"Base.h"
+class multi_transform {
+public:
+	vector<string> types;
+	vector<float> values;
+};
+
+void transform_image(Graphics& graphics, float angle, float width, float height, float scroll_x, float scroll_y, float scale);
+void read_transform(string value, multi_transform& tr);
+void apply_transform(LinearGradientBrush& lgbrush, multi_transform trans);
+
+
