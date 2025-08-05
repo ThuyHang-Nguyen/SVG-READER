80528f5be62f3a07a327e404b767a24aefd1bc01
ldkhoa306 <ldkhoa3006@gmail.com>
2025-08-05 23:34:56 +0700

    Gradient.h

--- Gradient.h
+++ Gradient.h
@@ -1,5 +1,6 @@
+//Gradient.h declares hierarchy of classes for storing gradient information and related functions.
 #pragma once
-#include "Base.h"
+#include "Viewbox.h"
 class stop {
 public:
 	color stop_color;
@@ -10,41 +11,3 @@ public:
 		stop_opacity = 1;
 	}
 };
-
-class Gradient {
-public:
-	vector<stop> stop_list;
-	float* get_point_list();
-	Color* get_color_list();
-};
-
-class linearGradient : public Gradient {
-public:
-	string id;
-	point start, end;
-	string units;
-	bool percentage;
-};
-
-class radialGradient : public Gradient {
-public:
-	string id;
-	point center;
-	float r, fx, fy;
-	string units, xlink_href;
-	radialGradient() {
-		r = fx = fy = 0;
-		xlink_href = "";
-	}
-};
-
-class defs {
-public:
-	vector<linearGradient> lg_list;
-	vector<radialGradient> rg_list;
-};
-
-//read function
-void readStop(string name, string value, stop& stop);
-void readLinearGradient(string name, string value, linearGradient& lg);
-void readRadialGradient(string name, string value, radialGradient& rg);
